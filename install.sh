476c8c46fb68dd424c769c9bc1568df332258012
hacker3983 <54384337+hacker3983@users.noreply.github.com>
2020-09-07 11:38:07 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,6 @@
-rm /etc/apt/sources.list
-echo "# 
-# 
-
-# deb cdrom:[Kali GNU/Linux 2020.1rc4 _Kali-last-snapshot_ - Official amd64 DVD Binary-1 with firmware 20200124-09>
-
-#deb cdrom:[Kali GNU/Linux 2020.1rc4 _Kali-last-snapshot_ - Official amd64 DVD Binary-1 with firmware 20200124-09:>
- 
-deb http://http.kali.org/kali kali-rolling main non-free contrib
-# deb-src http://http.kali.org/kali kali-rolling main non-free contrib
- 
-# This system was installed using small removable media
-# (e.g. netinst, live or single CD). The matching "deb cdrom"
-# entries were disabled at the end of the installation process.
-# For information about how to configure apt package sources,
-# see the sources.list(5) manual.
-deb http://ftp.debian.org/debian/ stretch main contrib non-free
-" >> /etc/apt/sources.list
-sudo apt-get update -y
+echo "do this nano /etc/apt/sources.list"
+sleep 40
+sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install python2.7-dev libssl-dev zlib1g-dev libpcap-dev -y
 sudo apt-get install libpcap-dev -y
