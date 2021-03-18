89130bd64494729af4316d27d05d948433db605b
hacker3983 <54384337+hacker3983@users.noreply.github.com>
2021-03-18 13:49:45 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,18 @@
-install_pyrit() {
-git clone https://github.com/JPaulMora/Pyrit.git && cd Pyrit
-python setup.py clean
-python setup.py build
-python setup.py install
-}
-banner="\033[0m[\033[32m+\033[0m]"
-echo -e "$banner Adding source to sources.list"
-echo deb http://ftp.debian.org/debian/ stretch main contrib non-free >> /etc/apt/sources.list
-
-echo -e "$banner Updating and Upgrading"
-apt-get update && apt-get upgrade -y
-
-echo -e "\033[0m[\033[32m+\033[0m] Installing required packages"
-apt-get install python2.7-dev libssl-dev zlib1g-dev libpcap-dev -y
-
-echo -e "\033[0m[\033[32m+\033[0m] Removing pyrit and cpyrit"
-apt-get remove --purge pyrit -y && rm -r /usr/local/lib/python2.7/dist-packages/cpyrit/
-echo -e "$banner Installing psycopg2 and scapy"
-pip install psycopg2 scapy
-
-echo -e "$banner Installing python-scapy"
-apt-get install python-scapy -y
-echo -e "$banner Downloading and Installing Pyrit"
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install python2.7-dev libssl-dev zlib1g-dev libpcap-dev -y
+sudo apt-get install libpcap-dev -y
+apt-get remove --purge pyrit
+rm -r /usr/local/lib/python2.7/dist-packages/cpyrit/
+sudo pip install psycopg2
+sudo pip install scapy
+sudo apt-get install python-scapy -y
+echo "Downloading Pyrit"
 printf '\033]2;Downloading Pyrit\a'
-git clone https://github.com/JPaulMora/Pyrit.git && cd Pyrit
+git clone https://github.com/JPaulMora/Pyrit.git
+cd Pyrit 
 python setup.py clean
 python setup.py build
-python setup.py install
+sudo python setup.py install
 echo "Installation finished"
 printf '\033]2; Installation finished\a'
